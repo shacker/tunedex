24478a9ba844b3448a23e8fe71df15061cd801a1
Scot Hacker <shacker@birdhouse.org>
2017-05-11 23:22:17 -0700

    Use persistent_id as pk and track identifier

--- pyrex/urls.py
+++ pyrex/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     url(r'^artists/(?P<pk>\d+)$', views.ArtistDetailView.as_view(), name='artist_detail'),
 
     url(r'^tracks/$', views.TrackListView.as_view(), name='tracks'),
-    url(r'^tracks/(?P<pk>\d+)$', views.TrackDetailView.as_view(), name='track_detail'),
+    url(r'^tracks/(?P<pk>\w+)$', views.TrackDetailView.as_view(), name='track_detail'),
 
     url(r'^kinds/$', views.KindListView.as_view(), name='kinds'),
     url(r'^kinds/(?P<pk>\d+)$', views.KindDetailView.as_view(), name='kind_detail'),
