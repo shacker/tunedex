970cd5198d9e3edfd12a8eb3e254fe9f0c87cbfc
Scot Hacker <shacker@birdhouse.org>
2017-05-16 23:20:00 -0700

    Optionally limit sync to first n tracks

--- itl/management/commands/sync.py
+++ itl/management/commands/sync.py
@@ -13,7 +13,7 @@ from itl.models import Artist, Album, Track, Genre, Kind, TrackType, Playlist
 
 
 class Command(BaseCommand):
-    help = "Synchronize iTunes Library with local database"
+    help = "Synchronize iTunes Library with local database. Pass in limit as int to process partial library."
 
     def struct_to_datetime(self, struct_time):
         '''
@@ -26,7 +26,7 @@ class Command(BaseCommand):
             return None
 
     def add_arguments(self, parser):
-        parser.add_argument('playlist_args', nargs='?', type=str)
+        parser.add_argument('limit', nargs='?', type=int)
 
     def handle(self, *args, **options):
 
@@ -41,81 +41,82 @@ class Command(BaseCommand):
             pickle.dump(itl_source, open(pickle_file, "wb"))
         itl = pickle.load(open(pickle_file, "rb"))
 
-        # Either take playlist names from cli args, or get all
-        playlists = options['playlist_args'].split(",") if options['playlist_args'] else itl.getPlaylistNames()
+        # Optionally limit to a partial lib for testing
+        limit = options['limit'] if options['limit'] else 99999999999999
+        songcount = len(itl.songs)
+        for index, song in enumerate(itl.songs.items()):
+            if index > limit:
+                break
 
-        for pl in playlists:
-            print(pl)
-            for song in itl.getPlaylist(pl).tracks:
-                try:
-                    print("{a} - {n}".format(a=song.artist, n=song.name))
-                except:
-                    print("Track missing metadata")
+            # song obj is now e.g. `(15607, <pyItunes.Song.Song object at 0x109473e48>)``
+            song = song[1]
 
-                artist = album = genre = kind = None
+            print("{index}/{sc} {a} - {n}".format(a=song.artist, n=song.name, index=index, sc=songcount))
 
-                if song.artist or song.album_artist:
-                    artist_str = song.artist or song.album_artist
-                    artist, created = Artist.objects.get_or_create(name=artist_str)
+            artist = album = genre = kind = None
 
-                if song.album:
-                    # Quasi-bug: Each song will reset year and rating on album, which may or may not be correct
-                    album, created = Album.objects.get_or_create(
-                        title=song.album,
-                        defaults={'artist': artist, 'year': song.year, 'album_rating': song.album_rating})
+            if song.artist or song.album_artist:
+                artist_str = song.artist or song.album_artist
+                artist, created = Artist.objects.get_or_create(name=artist_str)
 
-                if song.genre:
-                    genre, created = Genre.objects.get_or_create(name=song.genre)
+            if song.album:
+                # Quasi-bug: Each song will reset year and rating on album, which may or may not be correct
+                album, created = Album.objects.get_or_create(
+                    title=song.album,
+                    defaults={'artist': artist, 'year': song.year, 'album_rating': song.album_rating})
 
-                if song.track_type:
-                    track_type, created = TrackType.objects.get_or_create(name=song.track_type)
+            if song.genre:
+                genre, created = Genre.objects.get_or_create(name=song.genre)
 
-                if song.kind:
-                    kind, created = Kind.objects.get_or_create(name=song.kind)
+            if song.track_type:
+                track_type, created = TrackType.objects.get_or_create(name=song.track_type)
 
-                track_data = {
-                    'album': album,
-                    'artist': artist,
-                    'bit_rate': song.bit_rate,
-                    'comments': song.comments,
-                    'compilation': song.compilation,
-                    'composer': artist,
-                    'date_added': self.struct_to_datetime(song.date_added),
-                    'date_modified': self.struct_to_datetime(song.date_modified),
-                    'disc_count': song.disc_count,
-                    'disc_number': song.disc_number,
-                    'genre': genre,
-                    'grouping': song.grouping,
-                    'kind': kind,
-                    'lastplayed': self.struct_to_datetime(song.lastplayed),
-                    'length': song.length,
-                    'loved': song.loved,
-                    'movement_count': song.movement_count,
-                    'movement_name': song.movement_name,
-                    'movement_number': song.movement_number,
-                    'play_count': song.play_count,
-                    'rating': song.rating,
-                    'sample_rate': song.sample_rate,
-                    'size': song.size,
-                    'skip_count': song.skip_count,
-                    'skip_date': self.struct_to_datetime(song.skip_date),
-                    'total_time': song.total_time,
-                    'title': song.name,
-                    'track_count': song.track_count,
-                    'track_id': song.track_id,
-                    'track_number': song.track_number,
-                    'track_type': track_type,
-                    'work': song.work,
-                    'year': song.year,
-                }
+            if song.kind:
+                kind, created = Kind.objects.get_or_create(name=song.kind)
 
-                track, created = Track.objects.update_or_create(
-                    persistent_id=song.persistent_id,
-                    defaults=track_data,
-                )
+            track_data = {
+                'album': album,
+                'artist': artist,
+                'bit_rate': song.bit_rate,
+                'comments': song.comments,
+                'compilation': song.compilation,
+                'composer': artist,
+                'date_added': self.struct_to_datetime(song.date_added),
+                'date_modified': self.struct_to_datetime(song.date_modified),
+                'disc_count': song.disc_count,
+                'disc_number': song.disc_number,
+                'genre': genre,
+                'grouping': song.grouping,
+                'kind': kind,
+                'lastplayed': self.struct_to_datetime(song.lastplayed),
+                'length': song.length,
+                'loved': song.loved,
+                'movement_count': song.movement_count,
+                'movement_name': song.movement_name,
+                'movement_number': song.movement_number,
+                'play_count': song.play_count,
+                'rating': song.rating,
+                'sample_rate': song.sample_rate,
+                'size': song.size,
+                'skip_count': song.skip_count,
+                'skip_date': self.struct_to_datetime(song.skip_date),
+                'total_time': song.total_time,
+                'title': song.name,
+                'track_count': song.track_count,
+                'track_id': song.track_id,
+                'track_number': song.track_number,
+                'track_type': track_type,
+                'work': song.work,
+                'year': song.year,
+            }
+
+            track, created = Track.objects.update_or_create(
+                persistent_id=song.persistent_id,
+                defaults=track_data,
+            )
 
         # Create playlists
-        plists = [itl.getPlaylist(p) for p in playlists]
+        plists = [itl.getPlaylist(p) for p in itl.getPlaylistNames()]
         print("\n{c} playlists found".format(c=len(plists)))
         for pl in plists:
             playlist, created = Playlist.objects.get_or_create(name=pl.name)
